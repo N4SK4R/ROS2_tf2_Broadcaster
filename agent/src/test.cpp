2846fde891a16dec092d22e7bd0816487447125c
Aditya Naskar <115721424+N4SK4R@users.noreply.github.com>
2024-07-04 10:21:56 +0530

    Add files via upload

--- agent/src/test.cpp
+++ agent/src/test.cpp
@@ -3,15 +3,9 @@
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("update_goal_server");
+    auto node = rclcpp::Node::make_shared("update_goal_node");
 
     geometry_msgs::msg::Pose start_pose;
-    start_pose.position.x = 0.0;
-    start_pose.position.y = 0.0;
-    start_pose.position.z = 0.0;
-    start_pose.orientation.x = 0.0;
-    start_pose.orientation.y = 0.0;
-    start_pose.orientation.z = 0.0;
     start_pose.orientation.w = 1.0;
 
     Agent_Robot agent(node, "agent_1", start_pose);
