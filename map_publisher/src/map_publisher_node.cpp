b13871c3a92606e77e1cddb0a0b33c7bd6ddecb2
capnmav77 <its.mastermind77@gmail.com>
2024-07-05 11:10:11 +0530

    stable map updation done

--- map_publisher/src/map_publisher_node.cpp
+++ map_publisher/src/map_publisher_node.cpp
@@ -1,5 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "my_robot_interfaces/msg/node_status.hpp" // Update with correct message header
+#include <my_robot_interfaces/srv/get_map.hpp>
+
 #include <vector>
 
 using namespace std::chrono_literals; // for time literals like 1s
@@ -13,6 +15,9 @@ public:
 
         // Create publisher for NodeStatus message on /mapp topic
         publisher_ = this->create_publisher<my_robot_interfaces::msg::NodeStatus>("/mapp", 10);
+        service_ = this->create_service<my_robot_interfaces::srv::GetMap>(
+            "/update_map", std::bind(&MapPublisherNode::get_map , this, std::placeholders::_1, std::placeholders::_2)
+        );
 
         // Initialize the 3D grid
         init_map();
@@ -27,6 +32,8 @@ public:
 private:
     rclcpp::Publisher<my_robot_interfaces::msg::NodeStatus>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr publish_timer_;
+    //service to get the map and update it 
+    rclcpp::Service<my_robot_interfaces::srv::GetMap>::SharedPtr service_;
 
     int num_levels_x;
     int num_levels_y;
@@ -37,10 +44,72 @@ private:
     // Initialize the grid map
     void init_map()
     {
-        global_map.resize(num_levels_x * num_levels_y * num_levels_z + 3, 1);
-        global_map[global_map.size() - 3] = num_levels_x;
-        global_map[global_map.size() - 2] = num_levels_y;
-        global_map[global_map.size() - 1] = num_levels_z;
+            // Define the three 10x10 matrices
+        int data1[10][10] = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, -1, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, -1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 0, 1, -1, 1, 1, -1, 1, 1},
+            {0, 0, 0, 0, 1, 0, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 0, 0, 1}
+        };
+
+        int data2[10][10] = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 0, -1, 0, 1, 1, 1, 0},
+            {1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, -1, 1, 1, 1, 1, 0},
+            {1, 1, 1, 0, 1, 0, 1, 1, 1, 0},
+            {1, 1, 1, 0, 1, 0, 0, 0, 0, 1},
+            {1, 1, 1, 0, -1, 0, 0, -1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        int data3[10][10] = {
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, -1, 1, 1, 1, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+            {0, 1, 1, 1, 0, 0, 0, 0, 1, 1},
+            {1, 1, 1, 1, -1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, -1, 1, 0, -1, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        
+        // Append data1
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                global_map.push_back(data1[i][j]);
+            }
+        }
+        
+        // Append data2
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                global_map.push_back(data2[i][j]);
+            }
+        }
+        
+        // Append data3
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                global_map.push_back(data3[i][j]);
+            }
+        }
+
+
+        // Append the dimensions of the matrices
+        global_map.push_back(10);
+        global_map.push_back(10);
+        global_map.push_back(3);
     }
 
     // Publishes the current state of the map
@@ -54,6 +123,20 @@ private:
         publisher_->publish(msg);
 
         RCLCPP_INFO(this->get_logger(), "Map published.");
+    }
+
+    //service to get the map and update it
+    void get_map(const std::shared_ptr<my_robot_interfaces::srv::GetMap::Request> request,
+        std::shared_ptr<my_robot_interfaces::srv::GetMap::Response> response)
+    {
+        RCLCPP_INFO(this->get_logger(), "Incoming request");
+
+        // Update the map
+        global_map = request->map;
+
+        // Send response
+        response->success = true;
+        RCLCPP_INFO(this->get_logger(), "Map updated");
     }
 };
 
